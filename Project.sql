b1271624287abed3f25516e74f60f5fd2a5c9be8
Sourav-Patteri <souravpatteri@gmail.com>
2019-12-03 01:46:34 -0500

    updated with comments table

--- Project.sql
+++ Project.sql
@@ -2,7 +2,7 @@
 -- SCHEMA  
 -- ----------------------------------------
 DROP DATABASE IF EXISTS `lokham`;
-CREATE DATABASE IF NOT EXISTS `lokham` DEFAULT CHARACTER SET utf8;
+CREATE DATABASE IF NOT EXISTS `lokham` DEFAULT CHARACTER SET UTF8;
 SHOW WARNINGS;
 USE `lokham`; 
 -- ----------------------------------------
@@ -16,16 +16,18 @@ CREATE TABLE IF NOT EXISTS `lokham`.`users` (
   `first_name` VARCHAR(25) NOT NULL DEFAULT '',
   `last_name` VARCHAR(25) NOT NULL DEFAULT '',
   `middle_name` VARCHAR(25) NULL DEFAULT '',
-  `phone` VARCHAR(16) NOT NULL,
+  `phone` VARCHAR(16) NULL,
   `email` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   `role` ENUM('admin', 'general') NOT NULL DEFAULT 'general',
   `created_at` DATETIME NULL,
   `updated_at` DATETIME NULL,
-  PRIMARY KEY (`id`)) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)) ENGINE = INNODB DEFAULT CHARSET=UTF8;
   
 SHOW WARNINGS;
 
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `middle_name`, `phone`, `email`, `password`, `role`, `created_at`, `updated_at`) VALUES
+(1, 'Sourav', 'Patteri', '', '7067910774', 'srv.ptr@gmail.com', '$2y$10$a8AHDsFblwOih1bUTWMtZeRoNbe1EFk9.o7eKNEJ1/lAHox9ZNtUi', 'general',  '2019-11-04 08:50:02', '2019-11-04 15:30:20');
 -- ----------------------------------------
 --   Table `issues`
 -- ----------------------------------------
@@ -35,9 +37,9 @@ CREATE TABLE IF NOT EXISTS `lokham`.`issues` (
 	`issue_id` INT NOT NULL AUTO_INCREMENT,
 	`user_id` INT NOT NULL,
 	`content` TEXT NOT NULL DEFAULT '',
-	`created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-	`updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-     PRIMARY KEY (`issue_id`)) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+	`created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+     PRIMARY KEY (`issue_id`)) ENGINE = INNODB DEFAULT CHARSET=UTF8;
      
 -- Constraints for table `issues`
 
@@ -48,8 +50,12 @@ CREATE TABLE IF NOT EXISTS `lokham`.`issues` (
   REFERENCES `lokham`.`users` (`id`)
   ON UPDATE CASCADE
   ON DELETE CASCADE;
+  
+INSERT INTO `issues` (`issue_id`, `user_id`, `content`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Earth is dying', '2019-11-04 08:50:02', '2019-11-04 15:30:20');
+
 -- ----------------------------------------
---   Table `reviews`
+--   Table `ratings`
 -- ----------------------------------------
  
 DROP TABLE IF EXISTS `ratings`;
@@ -57,26 +63,67 @@ CREATE TABLE IF NOT EXISTS `lokham`.`ratings` (
 	`rating_id` INT NOT NULL AUTO_INCREMENT,
 	`user_id` INT NOT NULL,
 	`issue_id` INT NOT NULL,
-	`review` ENUM('1', '2', '3', '4', '5') NOT NULL,
-	`created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-	`updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-     PRIMARY KEY (`rating_id`)) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+	`rating` ENUM('1', '2', '3', '4', '5') NOT NULL,
+	`created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+     PRIMARY KEY (`rating_id`)) ENGINE = INNODB DEFAULT CHARSET=UTF8;
      
 -- Constraints for table `ratings`
 
   ALTER TABLE `lokham`.`ratings`
   ADD INDEX `fk_users_idx` (`user_id` ASC),
-  ADD CONSTRAINT `fk_user`
+  ADD CONSTRAINT `fk_rating_user`
   FOREIGN KEY (`user_id`)
   REFERENCES `lokham`.`users` (`id`)
   ON UPDATE CASCADE
-  ON DELETE CASCADE
+  ON DELETE CASCADE,
   ADD INDEX `fk_issues_idx` (`issue_id` ASC),
-  ADD CONSTRAINT `fk_issue`
+  ADD CONSTRAINT `fk_rating_issue`
   FOREIGN KEY (`issue_id`)
   REFERENCES `lokham`.`issues` (`issue_id`)
   ON UPDATE CASCADE
   ON DELETE CASCADE;
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `middle_name`, `phone`, `email`, `password`, `role`, `created_at`, `updated_at`) VALUES
-(1, 'Sourav', 'Patteri', '', '7067910774', 'srv.ptr@gmail.com', '$2y$10$a8AHDsFblwOih1bUTWMtZeRoNbe1EFk9.o7eKNEJ1/lAHox9ZNtUi', 'general',  '2019-11-04 08:50:02', '2019-11-04 15:30:20');
+INSERT INTO `ratings` (`rating_id`, `user_id`, `issue_id`, `rating`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, '5', '2019-11-04 08:50:02', '2019-11-04 15:30:20');
+-- ----------------------------------------
+--   Table `comments`
+-- ----------------------------------------
+
+DROP TABLE IF EXISTS `comments`;
+SHOW WARNINGS;
+
+CREATE TABLE IF NOT EXISTS `lokham`.`comments` (
+  `comment_id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `issue_id` INT NOT NULL,
+  `rating_id` INT NOT NULL,
+  `comments` TEXT NULL DEFAULT '',
+  `created_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`comment_id`)) ENGINE = INNODB DEFAULT CHARSET=UTF8;
+  
+  -- Constraints for table `comments` 
+
+  ALTER TABLE `lokham`.`comments` 
+  ADD INDEX `fk_users_idx` (`user_id` ASC),
+  ADD CONSTRAINT `fk_comment_user`
+  FOREIGN KEY (`user_id`)
+  REFERENCES `lokham`.`users` (`id`)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE,
+  ADD INDEX `fk_issues_idx` (`issue_id` ASC),
+  ADD CONSTRAINT `fk_comment_issue`
+  FOREIGN KEY (`issue_id`)
+  REFERENCES `lokham`.`issues` (`issue_id`)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE,
+  ADD INDEX `fk_ratings_idx` (`rating_id` ASC),
+  ADD CONSTRAINT `fk_comment_rating`
+  FOREIGN KEY (`rating_id`)
+  REFERENCES `lokham`.`ratings` (`rating_id`)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
+  
+INSERT INTO `comments` (`comment_id`, `user_id`, `issue_id`, `rating_id`, `comments`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 1, 'And unless we act right now, we cannot save it', '2019-11-04 08:50:02', '2019-11-04 15:30:20');
