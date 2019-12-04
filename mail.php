e781f1e0bf6240dbe15f9d0d22d06778d4bea8db
TavaresDev <vtavares.andre@gmail.com>
2019-12-03 23:00:12 -0400

    create contact form

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php 
+
+// $name = $_POST['contact_name'];
+// $email = $_POST['contact_email'];
+// $message = $_POST['contact_message'];
+// $formcontent="From: $name \n Message: $message";
+// $recipient = "contact@andretavares.dev";
+// $subject = "Contact Form";
+// $mailheader = "From: $email \r\n";
+// mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+// echo "Thank You!";
+?>
