173bd6331ece5bf0d395a81732a959658853ad4d
rowena-johnson <200425021@student.georgianc.on.ca>
2019-12-04 17:13:24 -0500

    about

--- about.php
+++ about.php
@@ -8,11 +8,21 @@
 <div class="container">
   <header class="mt-5">
     <h1>
-        Lorem Ipsum
+        About Lokham
     <br>
-      <small>Lorem ipsum</small>
+      <small>Strengthening social awareness and empathy</small>
     </h1>
-  </header>
+    <br>
+    </header>
+    <p>A social issue is an issue that impacts an impressive number of people. It is a part of society that individuals are worried about and might want to get it changed. The worry of a social issue might be objective or abstract. A social issue emerges because of differing assessments of people dependent on what is seen as right. Social issue doesn't really allude to an evil to be explained, however it might be a point to be talked about. Social issues can be distinctive for various gatherings, social orders, nations and world.<br>
+    <br>
+    There are a variety of methods to resolve variety of social issues. These include political processes, non-profit organizations, 
+    community groups and volunteering to have public dialogue on a social issue and arrive at a most acceptable solution.The Lokham provides a platform to race your voice against social concerns.
+    The power of Lokham lies in creating networks of people facing similar issues, quickly enabling them to come up with answers and  will engage with the most critical issues of our time – from the escalating climate 
+    catastrophe to widespread inequality to the influence of big tech on our lives. At a time when factual information is a necessity, we believe that each of us, around the world, deserves access to accurate reporting with integrity at its heart.
+    We hope you will consider supporting us today. We need your support to keep delivering reviews about each problem that’s open and independent.  Every reader contribution, however big or small, is so valuable.
+    </p>
+  
 </div>
 
 <?php include(ROOT . '/partials/_footer.php') ?>
