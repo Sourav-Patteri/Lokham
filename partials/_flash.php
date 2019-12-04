c049398a79c7e9004a29af0fbcf03b570885dab9
TavaresDev <vtavares.andre@gmail.com>
2019-12-04 00:40:19 -0400

    fix bug in the flash, why??? have now ideia

--- partials/_flash.php
+++ partials/_flash.php
@@ -1,7 +1,7 @@
 <?php
   if (!empty($flash_data)) {
     foreach($flash_data as $type => $messages) {
-      echo "<div class='alert alert-{$type}'>";
+      echo "<div class='alert mb-0 alert-{$type}'>";
       foreach ($messages as $message) {
         echo "{$message}<br>";
       }
