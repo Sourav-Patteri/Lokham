e1f15bf531dadb1c7a927d903dd11f562cb0b2f9
Sourav-Patteri <souravpatteri@gmail.com>
2019-12-07 21:15:44 -0500

    working image uploader

--- issues/create.php
+++ issues/create.php
@@ -44,7 +44,7 @@
   ?>
 
   <?php
-  $target_dir = "uploads/";
+  $target_dir = "../uploads/";
   $target_file = $target_dir . basename($_FILES["uploads"]["name"]);
   $uploadOk = 1;
   $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
